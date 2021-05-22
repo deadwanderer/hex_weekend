fcdf1d55a371d1a5af48ca2ca0240369d95cd55f
deadwanderer <anthonyjessica822@gmail.com>
2021-05-22 07:31:10 -0400

    Completed initial setup

---
+++ include/Camera.h
@@ -0,0 +1,6 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "HandmadeMath.h"
+
+#endif  // CAMERA_H
---
+++ src/shaders.glsl
@@ -0,0 +1,21 @@
+@ctype vec2 hmm_vec2
+@ctype vec3 hmm_vec3
+@ctype vec4 hmm_vec4
+@ctype mat4 hmm_mat4
+
+@vs vs
+  layout(location = 0) in vec3 pos;
+  void main() {
+    gl_Position = pos;
+  }
+@end
+
+@fs fs
+out vec4 FragColor;
+
+void main() {
+  FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+}
+@end
+
+@program Shader vs fs
